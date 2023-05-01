c264b4cd4e7a551433cf9cbb7f8d226b1156f30f
Pierre-Gei <pierregeiguer@gmail.com>
2023-05-01 16:01:15 +0200

    modifs variables

--- kmeans n-dim.py
+++ kmeans n-dim.py
@@ -3,69 +3,67 @@ import csv
 import visualisation
 
 
-def getDataset(file):
+def getDataset(fichier):
     # data as numpy array
-    data = np.genfromtxt(file, delimiter=',', skip_header=1)
+    data = np.genfromtxt(fichier, delimiter=',', skip_header=1)
     dimention = data.shape[1]
     return data, dimention
 
 
-def saveResult(file, data, dimention):
-    with open(file, 'w', newline='') as csvfile:
+def saveResult(fichier, data, dimention):
+    with open(fichier, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
-        # write header like this: "x1, x2, x3, ..., centroid_x1, centroid_x2, centroid_x3, ..." depending on the dimention
+        # header : x1, x2, x3, ... , centroid_x1, centroid_x2, centroid_x3, ... selon la dimention
         writer.writerow(["x" + str(i) for i in range(dimention)] + ["centroid_x" + str(i) for i in range(dimention)])
         for row in data:
             writer.writerow(row)
 
 def kmeans(data, dimention, NB_CLUSTERS, MAX_ITER):
+    #initialisation
     centroids = data[np.random.choice(data.shape[0], NB_CLUSTERS, replace=False), :]
     clusters = np.array([])
     iteration = 0
     convergence = False
-    old_mean_distance = float('inf')
-    mean_distances = float('inf')
+    old_distance_moyenne = float('inf')
+    distance_moyenne = float('inf')
 
     while not convergence and iteration < MAX_ITER:
-        # assign each point to the closest centroid
-        # create a list of clusters
+        #creation des clusters
         distances = np.linalg.norm(data[:, np.newaxis, :] - centroids, axis=2)
         clusters = np.argmin(distances, axis=1)
 
-        # compute the new centroids
+        # calcul des nouveaux centroids
         new_centroids = np.array([np.mean(data[clusters == i], axis=0) for i in range(NB_CLUSTERS)])
 
-        # check convergence
-        mean_distance = np.mean(np.linalg.norm(centroids - new_centroids, axis=1))
-        if mean_distance == old_mean_distance:
+        # verification de la convergence
+        distance_moyenne = np.mean(np.linalg.norm(centroids - new_centroids, axis=1))
+        if distance_moyenne == old_distance_moyenne:
             convergence = True
         else:
-            old_mean_distance = mean_distance
-        point_centroid_distances = np.linalg.norm(data - centroids[clusters], axis=1)
-        mean_point_centroid_distance = np.mean(point_centroid_distances)
+            old_distance_moyenne = distance_moyenne
+        distance_point_centroid = np.linalg.norm(data - centroids[clusters], axis=1)
+        distance_moyenne_point_centroid = np.mean(distance_point_centroid)
         centroids = new_centroids
         iteration += 1
-        mean_distances = mean_distance
 
-    # format data for visualisation in a single array like this: [[x1, y1, centroid_x, centroid_y], [x2, y2, centroid_x, centroid_y], ...]
+    # formattage pour la visualisation [[x1, y1, centroid_x, centroid_y], [x2, y2, centroid_x, centroid_y], ...]
     visualisation_data = np.concatenate((data, centroids[clusters]), axis=1)
-    return visualisation_data, mean_point_centroid_distance
+    return visualisation_data, distance_moyenne_point_centroid
 
 def runKmeans(data, dimention, NB_CLUSTERS, MAX_ITER, NB_RUNS):
-    # run kmeans NB_RUNS times and keep the best result
-    best_result = None
-    best_mean_point_centroid_distance = float('inf')
+    # run kmeans NB_run de fois et garde le meilleur resultat
+    meilleur_resultat = None
+    meilleure_distance_moyenne_point_centroids = float('inf')
     for i in range(NB_RUNS):
-        visualisation_data, mean_point_centroid_distance = kmeans(data, dimention, NB_CLUSTERS, MAX_ITER)
-        if mean_point_centroid_distance < best_mean_point_centroid_distance:
-            best_mean_point_centroid_distance = mean_point_centroid_distance
-            best_result = visualisation_data
-    print("best mean point centroid distance: " + str(best_mean_point_centroid_distance))
-    return best_result
+        visualisation_data, distance_moyenne_point_centroids = kmeans(data, dimention, NB_CLUSTERS, MAX_ITER)
+        if distance_moyenne_point_centroids < meilleure_distance_moyenne_point_centroids:
+            meilleure_distance_moyenne_point_centroids = distance_moyenne_point_centroids
+            meilleur_resultat = visualisation_data
+    print("moyenne de distance point_centroids " + str(meilleure_distance_moyenne_point_centroids))
+    return meilleur_resultat
 
 NB_CLUSTERS = 10
 MAX_ITER = 100
-NB_THREADS = 4
 NB_RUNS = 10
 
 data, dimention = getDataset("./data/3d_data.csv")
